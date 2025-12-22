e23058963c0cd01c830679c40b6d2fe8deb1cc33
Auwal17 <auwalsaneeabdullahi@gmail.com>
2025-12-22 09:47:54 +0100

    Add student visualization script, dataset, and plot

---
+++ visualisation/students_visualisation.R
@@ -0,0 +1,9 @@
+library(ggplot2)  # load ggplot2
+
+students <- read.csv("students.csv")  # load CSV
+
+head(students)  # view first few rows
+str(students)   # see column names and data types
+
+
+
